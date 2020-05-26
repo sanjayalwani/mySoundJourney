56fb0b9dad069747c7214da5d33c682f88e533eb
Sanjay <priv.sanjay@gmail.com>
2020-05-25 21:17:48 -0700

    Added routing logic and structure, created main layout components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import logo from './logo.svg';
-import Navigation from './components/Navigation';
+
+import Navigation from './components/MainUI/Navigation';
 import Landing from './pages/Landing';
-import Footer from './components/Footer';
+import Footer from './components/MainUI/Footer';
+import Journey from './pages/Journey';
+
+//Add Oauth here with contextual routing based on authorization ( has access token )
+//If !auth then redirect to "/auth" which contains landing page
+//If auth then 
+//default is "/" for recent tracks called JOURNEY
+//path "/top" is modified by "/artists" or "/tracks" called TOP 50
+//path "/saved" is modified by playlists albums artists songs etc. called SAVED
 
 function App() {
   return (
@@ -14,6 +22,10 @@ function App() {
           <Route path="/" exact>
             <Landing />
           </Route>
+          <Route path="/journey" >
+            <Journey />
+          </Route>
+          <Redirect to="/" />
         </Switch>
       </main>
       <Footer />
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './Landing.css'
 
 const Landing = props => {
     return(
         <div className="auth-container">
-            <h2 className="auth-container__description">Get started by authorizing yourself with spotify</h2>
-            <button className="auth-container__button">CONNECT TO SPOTIFY</button>
+            <h2 className="auth-container__description">Get started by authorizing yourself with Spotify</h2>
+            <Link className="auth-container__button" to="/journey">CONNECT TO SPOTIFY</Link>
         </div>
     );
 }
