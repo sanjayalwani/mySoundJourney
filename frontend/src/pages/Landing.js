848c02b692ba467fc21ce2c00661e84120bd16e8
Sanjay <priv.sanjay@gmail.com>
2020-07-01 00:03:06 -0700

    Changed hyperlinks to represent change in environment

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -3,10 +3,11 @@ import React from 'react';
 import './Landing.css'
 
 const Landing = props => {
+    const BASE_URL = document.location.href;
     return(
         <div className="auth-container">
             <h2 className="auth-container__description">Get started by authorizing yourself with Spotify</h2>
-            <a className="auth-container__button" href="http://localhost:8888/auth/login">CONNECT TO SPOTIFY</a>
+            <a className="auth-container__button" href={`${BASE_URL}auth/login`}>CONNECT TO SPOTIFY</a>
         </div>
     );
 }
