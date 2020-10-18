62a62a846f1ace7ed359fdad27ac7e19dc106dce
Sanjay <priv.sanjay@gmail.com>
2020-10-17 19:46:01 -0700

    improve our landing page with action first

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -6,6 +6,9 @@ const Landing = props => {
     const BASE_URL = "";//document.location.href;
     return(
         <div className="auth-container">
+            <h2 className="auth-container__description">Get in tune with yourself.</h2>
+            <a className="auth-container__button" href={`${BASE_URL}auth/login`}>CONNECT TO SPOTIFY</a>
+            <hr />
             <h2>
                 Ever been impressed by a song recommended to you on Spotify? Or are you itching
                 for more insights into your listening habits like Wrapped 2019?
@@ -13,16 +16,14 @@ const Landing = props => {
             <br></br>
             <h3 className="msj-features">
                 mySoundJourney features
-                <ul className="msj-features-list">
+                <ul className="msj-features_list">
                     <li>Recent track history</li>
                     <li>Energy charts</li>
                     <li>Positivity charts</li>
                     <li>... and more in development!</li>
                 </ul>
             </h3>
-            <hr />
-            <h2 className="auth-container__description">Get in tune with yourself.</h2>
-            <a className="auth-container__button" href={`${BASE_URL}auth/login`}>CONNECT TO SPOTIFY</a>
+            
         </div>
     );
 }
